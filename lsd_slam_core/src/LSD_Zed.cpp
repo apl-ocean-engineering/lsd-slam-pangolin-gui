ea6feaccf456b7299d471b78d5dcbcbbb61f02ad
Aaron Marburg <amarburg@apl.washington.edu>
2016-02-19 12:50:40 -0800

    Initial checkin of Zed version.

--- lsd_slam_core/src/LSD_Zed.cpp
+++ lsd_slam_core/src/LSD_Zed.cpp
@@ -2,7 +2,7 @@
 * This file is part of LSD-SLAM.
 *
 * Copyright 2013 Jakob Engel <engelj at in dot tum dot de> (Technical University of Munich)
-* For more information see <http://vision.in.tum.de/lsdslam> 
+* For more information see <http://vision.in.tum.de/lsdslam>
 *
 * LSD-SLAM is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,8 @@
 * You should have received a copy of the GNU General Public License
 * along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
 */
+
+#include <zed/Camera.hpp>
 
 #include "LiveSLAMWrapper.h"
 
@@ -40,153 +42,85 @@
 
 #include "GUI.h"
 
-std::vector<std::string> files;
 int w, h, w_inp, h_inp;
 ThreadMutexObject<bool> lsdDone(false);
 GUI gui;
 RawLogReader * logReader = 0;
+
 int numFrames = 0;
 
-std::string &ltrim(std::string &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-        return s;
-}
-std::string &rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-        return s;
-}
-std::string &trim(std::string &s) {
-        return ltrim(rtrim(s));
-}
-int getdir (std::string dir, std::vector<std::string> &files)
-{
-    DIR *dp;
-    struct dirent *dirp;
-    if((dp  = opendir(dir.c_str())) == NULL)
-    {
-        return -1;
-    }
-
-    while ((dirp = readdir(dp)) != NULL) {
-    	std::string name = std::string(dirp->d_name);
-
-    	if(name != "." && name != "..")
-    		files.push_back(name);
-    }
-    closedir(dp);
-
-
-    std::sort(files.begin(), files.end());
-
-    if(dir.at( dir.length() - 1 ) != '/') dir = dir+"/";
-	for(unsigned int i=0;i<files.size();i++)
-	{
-		if(files[i].at(0) != '/')
-			files[i] = dir + files[i];
-	}
-
-    return files.size();
-}
-
-int getFile (std::string source, std::vector<std::string> &files)
-{
-	std::ifstream f(source.c_str());
-
-	if(f.good() && f.is_open())
-	{
-		while(!f.eof())
-		{
-			std::string l;
-			std::getline(f,l);
-
-			l = trim(l);
-
-			if(l == "" || l[0] == '#')
-				continue;
-
-			files.push_back(l);
-		}
-
-		f.close();
-
-		size_t sp = source.find_last_of('/');
-		std::string prefix;
-		if(sp == std::string::npos)
-			prefix = "";
-		else
-			prefix = source.substr(0,sp);
-
-		for(unsigned int i=0;i<files.size();i++)
-		{
-			if(files[i].at(0) != '/')
-				files[i] = prefix + "/" + files[i];
-		}
-
-		return (int)files.size();
-	}
-	else
-	{
-		f.close();
-		return -1;
-	}
-
-}
+sl::zed::Camera *camera = NULL;
 
 using namespace lsd_slam;
 
 void run(SlamSystem * system, Undistorter* undistorter, Output3DWrapper* outputWrapper, Sophus::Matrix3f K)
 {
     // get HZ
-    double hz = 30;
+    double hz = camera->getCurrentFPS();
+    if( hz < 0 ) {
+      printf("Unable to get FPS from input, using 30\n");
+      hz = 30;
+    }
 
-    cv::Mat image = cv::Mat(h, w, CV_8U);
     int runningIDX=0;
     float fakeTimeStamp = 0;
 
-    for(unsigned int i = 0; i < numFrames; i++)
+    for(unsigned int i = 0; (numFrames < 0) || (i < numFrames); i++)
     {
         if(lsdDone.getValue())
             break;
 
-        cv::Mat imageDist = cv::Mat(h, w, CV_8U);
-
-        if(logReader)
-        {
-            logReader->getNext();
-
-            cv::Mat3b img(h, w, (cv::Vec3b *)logReader->rgb);
-
-            cv::cvtColor(img, imageDist, CV_RGB2GRAY);
-        }
-        else
-        {
-            imageDist = cv::imread(files[i], CV_LOAD_IMAGE_GRAYSCALE);
-
-            if(imageDist.rows != h_inp || imageDist.cols != w_inp)
-            {
-                if(imageDist.rows * imageDist.cols == 0)
-                    printf("failed to load image %s! skipping.\n", files[i].c_str());
-                else
-                    printf("image %s has wrong dimensions - expecting %d x %d, found %d x %d. Skipping.\n",
-                            files[i].c_str(),
-                            w,h,imageDist.cols, imageDist.rows);
-                continue;
-            }
+        // At present, just grab images
+        if( camera->grab( sl::zed::SENSING_MODE::RAW, false, false ) ) {
+          printf("Error reading data from camera" );
+          break;
         }
 
-        assert(imageDist.type() == CV_8U);
 
-        undistorter->undistort(imageDist, image);
+        sl::zed::Mat left = camera->retrieveImage(sl::zed::SIDE::LEFT);
+        cv::Mat imageRGB( left.width, left.height, CV_8UC4, left.data );
 
-        assert(image.type() == CV_8U);
+        // Convert to greyscale
+        cv::Mat imageGray( imageRGB.cols, imageRGB.rows, CV_8UC1 );
+        cv::cvtColor( imageRGB, imageGray, cv::COLOR_BGR2GRAY );
+
+        // if(logReader)
+        // {
+        //     logReader->getNext();
+        //
+        //     cv::Mat3b img(h, w, (cv::Vec3b *)logReader->rgb);
+        //
+        //     cv::cvtColor(img, imageDist, CV_RGB2GRAY);
+        // }
+        // else
+        // {
+        //     imageDist = cv::imread(files[i], CV_LOAD_IMAGE_GRAYSCALE);
+        //
+        //     if(imageDist.rows != h_inp || imageDist.cols != w_inp)
+        //     {
+        //         if(imageDist.rows * imageDist.cols == 0)
+        //             printf("failed to load image %s! skipping.\n", files[i].c_str());
+        //         else
+        //             printf("image %s has wrong dimensions - expecting %d x %d, found %d x %d. Skipping.\n",
+        //                     files[i].c_str(),
+        //                     w,h,imageDist.cols, imageDist.rows);
+        //         continue;
+        //     }
+        // }
+
+        // assert(imageDist.type() == CV_8U);
+        //
+        // undistorter->undistort(imageDist, image);
+
+        assert(imageGray.type() == CV_8U);
 
         if(runningIDX == 0)
         {
-            system->randomInit(image.data, fakeTimeStamp, runningIDX);
+            system->randomInit(imageGray.data, fakeTimeStamp, runningIDX);
         }
         else
         {
-            system->trackFrame(image.data, runningIDX, hz == 0, fakeTimeStamp);
+            system->trackFrame(imageGray.data, runningIDX, hz == 0, fakeTimeStamp);
         }
 
         gui.pose.assignValue(system->getCurrentPoseEstimateScale());
@@ -212,84 +146,116 @@ void run(SlamSystem * system, Undistorter* undistorter, Output3DWrapper* outputW
 
 int main( int argc, char** argv )
 {
-	// get camera calibration in form of an undistorter object.
+
+  // open image files: first try to open as file.
+	std::string source;
+	if( Parse::arg(argc, argv, "-f", source) > 0)
+	{
+		printf("Loading SVO file %s\n", source.c_str() );
+    camera = new sl::zed::Camera( source );
+	} else {
+    printf("Using live zed data\n");
+
+    const sl::zed::ZEDResolution_mode resolution = sl::zed::HD1080;
+    camera = new sl::zed::Camera( resolution );
+  }
+
+  // get camera calibration in form of an undistorter object.
 	// if no undistortion is required, the undistorter will just pass images through.
-	std::string calibFile;
+	// std::string calibFile;
 	Undistorter* undistorter = 0;
 
-	if(Parse::arg(argc, argv, "-c", calibFile) > 0)
-	{
-		 undistorter = Undistorter::getUndistorterForFile(calibFile.c_str());
-	}
+  sl::zed::ERRCODE err = camera->init( sl::zed::MODE::NONE, -1, true );
+  if (err != sl::zed::SUCCESS) {
+    printf( "Unable to init the zed: %s\n", errcode2str(err).c_str() );
+    delete camera;
+    exit(-1);
+  }
 
-	if(undistorter == 0)
-	{
-		printf("need camera calibration file! (set using -c FILE)\n");
-		exit(0);
-	}
+	// if(Parse::arg(argc, argv, "-c", calibFile) > 0)
+	// {
+	// 	 undistorter = Undistorter::getUndistorterForFile(calibFile.c_str());
+	// }
+  //
+	// if(undistorter == 0)
+	// {
+	// 	printf("need camera calibration file! (set using -c FILE)\n");
+	// 	exit(0);
+	// }
 
-	w = undistorter->getOutputWidth();
-	h = undistorter->getOutputHeight();
+	// w = undistorter->getOutputWidth();
+	// h = undistorter->getOutputHeight();
+  //
+	// w_inp = undistorter->getInputWidth();
+	// h_inp = undistorter->getInputHeight();
+  //
+	// float fx = undistorter->getK().at<double>(0, 0);
+	// float fy = undistorter->getK().at<double>(1, 1);
+	// float cx = undistorter->getK().at<double>(2, 0);
+	// float cy = undistorter->getK().at<double>(2, 1);
 
-	w_inp = undistorter->getInputWidth();
-	h_inp = undistorter->getInputHeight();
+  sl::zed::StereoParameters *params = camera->getParameters();
 
-	float fx = undistorter->getK().at<double>(0, 0);
-	float fy = undistorter->getK().at<double>(1, 1);
-	float cx = undistorter->getK().at<double>(2, 0);
-	float cy = undistorter->getK().at<double>(2, 1);
+  float fx = params->LeftCam.fx;
+	float fy = params->LeftCam.fy;
+	float cx = params->LeftCam.cx;
+	float cy = params->LeftCam.cy;
+
 	Sophus::Matrix3f K;
 	K << fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0;
-
-	Resolution::getInstance(w, h);
 	Intrinsics::getInstance(fx, fy, cx, cy);
+  //delete params;
+
+
+  {
+    sl::zed::resolution resolution = camera->getImageSize();
+
+    // Set up these two singletons
+  	Resolution::getInstance( resolution.width, resolution.height );
+  }
+
 
 	gui.initImages();
 
 	Output3DWrapper* outputWrapper = new PangolinOutput3DWrapper(w, h, gui);
 
+
 	// make slam system
 	SlamSystem * system = new SlamSystem(w, h, K, doSlam);
 	system->setVisualization(outputWrapper);
 
+  numFrames = camera->getSVONumberOfFrames();
 
-	// open image files: first try to open as file.
-	std::string source;
-	if(!(Parse::arg(argc, argv, "-f", source) > 0))
-	{
-		printf("need source files! (set using -f FOLDER or KLG)\n");
-		exit(0);
-	}
+	// Bytef * decompressionBuffer = new Bytef[Resolution::getInstance().numPixels() * 2];
+  // IplImage * deCompImage = 0;
 
-	Bytef * decompressionBuffer = new Bytef[Resolution::getInstance().numPixels() * 2];
-    IplImage * deCompImage = 0;
+    // if(source.substr(source.find_last_of(".") + 1) == "klg")
+    // {
+    //     logReader = new RawLogReader(decompressionBuffer,
+    //                                  deCompImage,
+    //                                  source);
+    //
+    //     numFrames = logReader->getNumFrames();
+    // }
+    // else
+    // {
+    //     if(getdir(source, files) >= 0)
+    //     {
+    //         printf("found %d image files in folder %s!\n", (int)files.size(), source.c_str());
+    //     }
+    //     else if(getFile(source, files) >= 0)
+    //     {
+    //         printf("found %d image files in file %s!\n", (int)files.size(), source.c_str());
+    //     }
+    //     else
+    //     {
+    //         printf("could not load file list! wrong path / file?\n");
+    //     }
+    //
+    //     numFrames = (int)files.size();
+    // }
 
-    if(source.substr(source.find_last_of(".") + 1) == "klg")
-    {
-        logReader = new RawLogReader(decompressionBuffer,
-                                     deCompImage,
-                                     source);
-
-        numFrames = logReader->getNumFrames();
-    }
-    else
-    {
-        if(getdir(source, files) >= 0)
-        {
-            printf("found %d image files in folder %s!\n", (int)files.size(), source.c_str());
-        }
-        else if(getFile(source, files) >= 0)
-        {
-            printf("found %d image files in file %s!\n", (int)files.size(), source.c_str());
-        }
-        else
-        {
-            printf("could not load file list! wrong path / file?\n");
-        }
-
-        numFrames = (int)files.size();
-    }
-
+  printf("Launching LSD thread\n");
 	boost::thread lsdThread(run, system, undistorter, outputWrapper, K);
 
 	while(!pangolin::ShouldQuit())
@@ -314,8 +280,10 @@ int main( int argc, char** argv )
 
 	lsdThread.join();
 
+  if( camera ) delete camera;
+
 	delete system;
-	delete undistorter;
+	if( undistorter ) delete undistorter;
 	delete outputWrapper;
 	return 0;
 }
