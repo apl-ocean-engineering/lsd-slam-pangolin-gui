54067d73f2e8ffd0a2c3e85905b244307ce71976
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-17 11:58:43 -0700

    Testing, perhaps I don't need GLEW at all.

--- cmake/BuildPangolin.cmake
+++ cmake/BuildPangolin.cmake
@@ -2,7 +2,6 @@
 SET( PANGOLIN_PREFIX_DIR ${PROJECT_BINARY_DIR}/Pangolin )
 SET( PANGOLIN_INSTALL_DIR ${PANGOLIN_PREFIX_DIR} )
 
-find_package( GLEW REQUIRED )
 find_package( GLUT REQUIRED )
 find_package( GLM REQUIRED )
 
@@ -12,13 +11,12 @@ ExternalProject_Add( Pangolin
                       BUILD_COMMAND ${EXTERNAL_PROJECT_MAKE_COMMAND}
                       CMAKE_CACHE_ARGS -DCMAKE_BUILD_TYPE:string=Release
                               -DCMAKE_INSTALL_PREFIX:path=${PANGOLIN_INSTALL_DIR}
-                              -DBUILD_EXAMPLES:bool=OFF -DFORCE_GLUT:bool=ON )
+                              -DBUILD_EXAMPLES:bool=OFF
+                              -DFORCE_GLUT:bool=ON )
 
-set( PANGOLIN_ROOT ${PANGOLIN_INSTALL_DIR} )
 set( Pangolin_LIBRARIES
       -L${PANGOLIN_INSTALL_DIR}/lib
-      pangolin
-      ${GLEW_LIBRARIES} )
+      pangolin )
 
 if( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
   LIST( APPEND Pangolin_LIBRARIES "-framework OpenGL")
