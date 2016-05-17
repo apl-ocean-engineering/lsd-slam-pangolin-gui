b020fc42a9eeac528fd92a99c99a4f3b397f4ab6
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-16 21:22:34 -0700

    I am an idiot who cannot write a shell script.

--- .travis/before_install_trusty.sh
+++ .travis/before_install_trusty.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 sudo apt-get update && sudo apt-get install -y cmake \
-		libopencv-dev libboost-all-dev libeigen3-dev
+		libopencv-dev libboost-all-dev libeigen3-dev \
 		libtclap-dev libgomp1 libsuitesparse-dev git
