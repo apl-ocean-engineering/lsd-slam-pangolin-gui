7973261e84f16faf2fd0e66a18b9263e35a77dbf
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-17 13:41:08 -0700

    Tried to make the OSX dep script tighter.

--- .travis/install_deps_osx.sh
+++ .travis/install_deps_osx.sh
@@ -3,9 +3,15 @@
 set -ev
 
 brew update
-brew install homebrew/science/opencv homebrew/science/suite-sparse
-brew install tclap eigen glew glm homebrew/x11/freeglut
-brew outdated cmake || brew upgrade cmake
+for pkg in homebrew/science/opencv homebrew/science/suite-sparse \
+					cmake tclap eigen glew glm homebrew/x11/freeglut
+do; brew outdated $pkg || brew upgrade $pkg; done
 
-## Need to patch Eigen 3.2.8
-patch -N -p1 -D /usr/local/include/ < eigen-3.2.8-patch.diff
+## Eigen 3.2.8 has a very minor bug
+# (http://eigen.tuxfamily.org/bz/show_bug.cgi?id=537)
+# that causes compilation problems on OSX.   A patch
+# has been accepted
+EIGEN_VER=`brew info eigen | head -1 | awk '{print $3}'`
+if [[ $EIGEN_VER = "3.2.8" ]]; then
+	patch --forward -p1 -d /usr/local/include/eigen3 < .travis/eigen-3.2.8-patch.diff
+fi
