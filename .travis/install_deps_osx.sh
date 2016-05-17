e89adc96d3f93b06af6ec39788cc65a6e0d88257
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-17 14:49:57 -0700

    Completed comment in install_deps_osx.sh

--- .travis/install_deps_osx.sh
+++ .travis/install_deps_osx.sh
@@ -10,7 +10,8 @@ do; brew outdated $pkg || brew upgrade $pkg; done
 ## Eigen 3.2.8 has a very minor bug
 # (http://eigen.tuxfamily.org/bz/show_bug.cgi?id=537)
 # that causes compilation problems on OSX.   A patch
-# has been accepted
+# has been accepted and is present in 3.2 HEAD as well
+# as devel
 EIGEN_VER=`brew info eigen | head -1 | awk '{print $3}'`
 if [[ $EIGEN_VER = "3.2.8" ]]; then
 	patch --forward -p1 -d /usr/local/include/eigen3 < .travis/eigen-3.2.8-patch.diff
