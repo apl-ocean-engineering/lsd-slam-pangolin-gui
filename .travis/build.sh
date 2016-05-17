eb57c95954f62574f7b1f9bc7e38cc9a5fde312c
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-17 13:58:30 -0700

    Added function to specify CMAKE_BUILD_TYPE to .travis/build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,9 @@
 
 set -ev
 
+BUILD_TYPE=${BUILD_TYPE:=Release}
+
 if [ ! -d build_ci ]; then mkdir build_ci; fi
 
-cmake -Bbuild_ci -H.
+cmake -Bbuild_ci -H. -DCMAKE_BUILD_TYPE:string=$BUILD_TYPE
 cd build_ci && make && make unit_test
