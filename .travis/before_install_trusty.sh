d2e636c66e4e99cf343a59355d6a981761caf9b9
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-16 21:48:51 -0700

    Added libglew-dev to Trusty dependencies.

--- .travis/before_install_trusty.sh
+++ .travis/before_install_trusty.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get update && sudo apt-get install -y cmake \
 		libopencv-dev libboost-all-dev libeigen3-dev \
-		libtclap-dev libgomp1 libsuitesparse-dev git
+		libtclap-dev libgomp1 libsuitesparse-dev git \
+		libglew-dev
