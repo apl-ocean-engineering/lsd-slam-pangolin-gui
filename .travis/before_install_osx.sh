d25c064f39dd1c65a60dcb826afe61c4a61a4265
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-16 22:39:34 -0700

    Added glm to brew dependencies as well.

--- .travis/before_install_osx.sh
+++ .travis/before_install_osx.sh
@@ -2,5 +2,5 @@
 
 brew update
 brew install homebrew/science/opencv homebrew/science/suite-sparse
-brew install tclap eigen glew
+brew install tclap eigen glew glm
 brew outdated cmake || brew upgrade cmake
