6dcd5a99080adee9e75ced7d48625459b9c0029f
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-17 12:59:53 -0700

    Updating Eigen ahead of 3.2.8

--- .travis/install_deps_osx.sh
+++ .travis/install_deps_osx.sh
@@ -6,3 +6,6 @@ brew update
 brew install homebrew/science/opencv homebrew/science/suite-sparse
 brew install tclap eigen glew glm homebrew/x11/freeglut
 brew outdated cmake || brew upgrade cmake
+
+## Need to patch Eigen 3.2.8
+patch -N -p1 -D /usr/local/include/ < eigen-3.2.8-patch.diff
