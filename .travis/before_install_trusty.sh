f798eba2adc69d61119a348c5d39b0e662eebe7c
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-16 22:35:58 -0700

    Added libglm-dev to Trusty dependencies.

--- .travis/before_install_trusty.sh
+++ .travis/before_install_trusty.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-sudo apt-get update && sudo apt-get install -y cmake \
+sudo apt-get update && \
+sudo apt-get install -y cmake \
 		libopencv-dev libboost-all-dev libeigen3-dev \
 		libtclap-dev libgomp1 libsuitesparse-dev git \
-		libglew-dev
+		libglew-dev libglm-dev
