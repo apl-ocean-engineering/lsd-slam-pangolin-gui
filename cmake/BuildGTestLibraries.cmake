5b213a930fa39c8bf1414b1bfb9302676e1f9fb3
Aaron Marburg <amarburg@apl.washington.edu>
2016-06-23 21:09:01 -0700

    Working out an inconsistency in GTest project makefile.

--- cmake/BuildGTestLibraries.cmake
+++ cmake/BuildGTestLibraries.cmake
@@ -18,8 +18,12 @@ ExternalProject_Add( gtest
 ExternalProject_Get_Property( gtest INSTALL_DIR )
 
 set( GTEST_INCLUDE_DIRS ${INSTALL_DIR}/include  )
-set( GTEST_LIBRARY ${INSTALL_DIR}/lib/libgtest.a  )
-set( GTEST_MAIN_LIBRARY ${INSTALL_DIR}/lib/libgtest_main.a  )
+set( GTEST_LIBRARY libgtest.a  )
+set( GTEST_MAIN_LIBRARY libgtest_main.a  )
 set( GTEST_BOTH_LIBRARIES ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY}  )
+
+set( GTEST_LIBRARY_DIR ${CMAKE_BINARY_DIR}/gtest/lib )
+link_directories( ${GTEST_LIBRARY_DIR} )
+# set( GTEST_BOTH_LIBRARIES gtest gtest_main )
 
 set_target_properties(gtest PROPERTIES EXCLUDE_FROM_ALL TRUE)
